fbae3aeec30f86d75d49268dce4e8ecd3f4bf9d8
otter606 <otter606@f0624e06-9363-4a27-9f68-80170093e2a0>
2009-01-26 11:10:27 +0000

    tests all example file copied from project example page

---
+++ sed-ml/jlibsedml/trunk/test/org/miase/jlibsedml/api/TestAllExamples.java
@@ -0,0 +1,51 @@
+package org.miase.jlibsedml.api;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAllExamples {
+
+	final File TEST_DIR = new File("TestData/"); 
+	List<File> filtered = new ArrayList<File>();
+	@Before
+	public void setUp() throws Exception {
+		File [] allfiles = TEST_DIR.listFiles();
+		
+		for (File f: allfiles){
+			if(f.getName().startsWith("sed") && 
+				f.getName().matches(".+\\.xml$") ){
+				
+			  filtered.add(f);
+			}
+		}
+	}
+	
+	@Test
+	public void testValidSedMLDocHasNoErrors() throws XMLException, IOException {
+		
+		
+		for (File testFile: filtered) {
+			System.out.println("here");
+		SEDMLDocument doc = Libsedml.readDocument(testFile.getAbsolutePath());
+		List<SedMLError> errors = doc.getErrors();
+		for (SedMLError error: errors) {
+			System.out.println(error.getMessage());
+		}
+		assertEquals(0, errors.size());
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
