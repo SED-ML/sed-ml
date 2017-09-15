03f8037459686462d2d17505a1a46cafeeae3df4
Matthias Koenig <konigmatt@googlemail.com>
2017-09-15 15:52:26 +0200

    working on omex creation for combine examples

---
+++ specification/level-1-version-3/examples/README.md
@@ -0,0 +1,16 @@
+# Specification examples
+This folder contains the specification examples and scripts to 
+create the combine archives from them.
+
+The combine archives can be created from the updated examples
+by running the `create_omex.py` script. The omex files are stored in
+the `__omex__` folder.
+
+# Create Combine Archives
+
+```
+mkvirtualenv sedml-examples
+(sedml-examples) pip install -r requirements.txt
+
+```
+
---
+++ specification/level-1-version-3/examples/create_omex.py
@@ -0,0 +1,136 @@
+"""
+Creates the Combine Archives from the folders.
+"""
+from __future__ import print_function, absolute_import
+import os
+import json
+import libcombine
+import pprint
+
+examples_dir = os.path.dirname(os.path.realpath(__file__))
+omex_dir = os.path.join(examples_dir, "./__omex__")
+
+# The following archives are created
+folders = ['2d-parameter-scan']
+
+def create_omex(folder, omex_file):
+    """ Creates a combine archive from folder
+
+    :param folder:
+    :param omex_file:
+    :return:
+    """
+    print('*' * 100)
+    print('Create OMEX:')
+    print('\t', folder)
+    print('*' * 100)
+
+    json_manifest = os.path.join(folder, 'manifest.json')
+    print(json_manifest)
+    with open(json_manifest, "r") as f:
+        json_entries = json.load(f)
+        json_entries = json_entries['entries']
+        pprint.pprint(json_entries)
+
+    time_now = libcombine.OmexDescription.getCurrentDateAndTime()
+    archive = libcombine.CombineArchive()
+    for entry in json_entries:
+        location = entry['location']
+        path = os.path.join(folder, location)
+        format = entry['format']
+        master = entry.get('master', False)
+        print(path, location, format, master)
+
+        print(type(path), type(location), type(format), type(master))
+        archive.addFile(path, location, format, master)
+
+        description = entry.get("description", None)
+        creators = entry.get("creators", None)
+
+        if description or creators:
+            omex_d = libcombine.OmexDescription()
+            omex_d.setAbout(location)
+            omex_d.setCreated(time_now)
+
+            if description:
+                omex_d.setDescription(description)
+
+            if creators:
+                for c in creators:
+                    creator = libcombine.VCard()
+                    creator.setFamilyName(c.get("familyName", ""))
+                    creator.setGivenName(c.get("givenName", ""))
+                    creator.setEmail(c.get("email", ""))
+                    creator.setOrganization(c.get("organisation"))
+                    omex_d.addCreator(creator)
+
+            archive.addMetadata(location, omex_d)
+
+    # delete the old omex file
+    if os.path.exists(omex_file):
+        os.remove(omex_file)
+
+    archive.writeToFile(omex_file)
+    print('Archive created:', omex_file)
+
+
+def printMetaDataFor(archive, location):
+    """ Prints metadata for given location.
+
+    :param archive: CombineArchive instance
+    :param location:
+    :return:
+    """
+    desc = archive.getMetadataForLocation(location)
+    if desc.isEmpty():
+        print("  no metadata for '{0}'".format(location))
+        return None
+
+    print("  metadata for '{0}':".format(location))
+    print("     Created : {0}".format(desc.getCreated().getDateAsString()))
+    for i in range(desc.getNumModified()):
+        print("     Modified : {0}".format(desc.getModified(i).getDateAsString()))
+
+    print("     # Creators: {0}".format(desc.getNumCreators()))
+    for i in range(desc.getNumCreators()):
+        creator = desc.getCreator(i)
+        print("       {0} {1}".format(creator.getGivenName(), creator.getFamilyName()))
+
+
+def printArchive(fileName):
+    """ Prints content of combine archive
+
+    :param fileName: path of archive
+    :return: None
+    """
+    archive = libcombine.CombineArchive()
+    if archive.initializeFromArchive(fileName) is None:
+        print("Invalid Combine Archive")
+        return None
+
+    print('*'*80)
+    print('Print archive:', fileName)
+    print('*' * 80)
+    printMetaDataFor(archive, ".")
+    print("Num Entries: {0}".format(archive.getNumEntries()))
+
+    for i in range(archive.getNumEntries()):
+        entry = archive.getEntry(i)
+        print(" {0}: location: {1} format: {2}".format(i, entry.getLocation(), entry.getFormat()))
+        printMetaDataFor(archive, entry.getLocation())
+
+        # the entry could now be extracted via
+        # archive.extractEntry(entry.getLocation(), <filename or folder>)
+
+        # or used as string
+        # content = archive.extractEntryToString(entry.getLocation());
+
+    archive.cleanUp()
+
+if __name__ == "__main__":
+    test_dir = os.path.join(examples_dir, folders[0])
+    test_omex = os.path.join(omex_dir, "{}.omex".format(folders[0]))
+
+    create_omex(test_dir, test_omex)
+    printArchive(test_omex)
+
